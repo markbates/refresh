markbates/refresh

commit 457c415453d6b663690cb1ee58ec870316caa568

Add a batcher to wait for changes

refresh/manager.go
@@ -44,16 +44,24 @@ func NewWithContext(c *Configuration, ctx context.Context) *Manager {
 func (r *Manager) Start() error {
 	w := NewWatcher(r)
 	w.Start()
-	go r.build(fsnotify.Event{Name: ":start:"})
+	go r.build([]fsnotify.Event{fsnotify.Event{Name: ":start:"}})
+	tick := time.Tick(r.BuildDelay)
+	events := make([]fsnotify.Event, 0)
 	go func() {
 		for {
 			select {
 			case event := <-w.Events:
 				if event.Op != fsnotify.Chmod {
-					go r.build(event)
+					events = append(events, event)
 				}
 				w.Remove(event.Name)
 				w.Add(event.Name)
+			case <-tick:
+				if len(events) == 0 {
+					continue
+				}
+				go r.build(events)
+				events = make([]fsnotify.Event, 0)
 			case <-r.context.Done():
 				break
 			}
@@ -73,16 +81,20 @@ func (r *Manager) Start() error {
 	return nil
 }
 
-func (r *Manager) build(event fsnotify.Event) {
+func (r *Manager) build(events []fsnotify.Event) {
 	r.gil.Do(func() {
 		defer func() {
 			r.gil = &sync.Once{}
 		}()
 		r.buildTransaction(func() error {
 			// time.Sleep(r.BuildDelay * time.Millisecond)
+			eventNames := make([]string, 0)
+			for _, event := range events {
+				eventNames = append(eventNames, event.Name)
+			}
 
 			now := time.Now()
-			r.Logger.Print("Rebuild on: %s", event.Name)
+			r.Logger.Print("Rebuild on: %s", strings.Join(eventNames, ", "))
 			cmd := exec.Command("go", "build", "-v", "-i", "-o", r.FullBuildPath())
 			err := r.runAndListen(cmd)
 			if err != nil {

commit 0b2bb8035576fb5fffcee9eed3233be1abdc9098

fully reverted 457c415453d6b663690cb1ee58ec870316caa568 which caused HUGE CPU usage!!

refresh/manager.go
@@ -44,24 +44,16 @@ func NewWithContext(c *Configuration, ctx context.Context) *Manager {
 func (r *Manager) Start() error {
 	w := NewWatcher(r)
 	w.Start()
-	go r.build([]fsnotify.Event{fsnotify.Event{Name: ":start:"}})
-	tick := time.Tick(r.BuildDelay)
-	events := make([]fsnotify.Event, 0)
+	go r.build(fsnotify.Event{Name: ":start:"})
 	go func() {
 		for {
 			select {
 			case event := <-w.Events:
 				if event.Op != fsnotify.Chmod {
-					events = append(events, event)
+					go r.build(event)
 				}
 				w.Remove(event.Name)
 				w.Add(event.Name)
-			case <-tick:
-				if len(events) == 0 {
-					continue
-				}
-				go r.build(events)
-				events = make([]fsnotify.Event, 0)
 			case <-r.context.Done():
 				break
 			}
@@ -81,21 +73,17 @@ func (r *Manager) Start() error {
 	return nil
 }
 
-func (r *Manager) build(events []fsnotify.Event) {
+func (r *Manager) build(event fsnotify.Event) {
 	r.gil.Do(func() {
 		defer func() {
 			r.gil = &sync.Once{}
 		}()
 		r.buildTransaction(func() error {
 			// time.Sleep(r.BuildDelay * time.Millisecond)
-			eventNames := make([]string, 0)
-			for _, event := range events {
-				eventNames = append(eventNames, event.Name)
-			}
 
 			now := time.Now()
-			r.Logger.Print("Rebuild on: %s", strings.Join(eventNames, ", "))
-			cmd := exec.Command("go", "build", "-v", "-i", "-o", r.FullBuildPath(), r.Configuration.BuildTargetPath)
+			r.Logger.Print("Rebuild on: %s", event.Name)
+			cmd := exec.Command("go", "build", "-v", "-i", "-o", r.FullBuildPath())
 			err := r.runAndListen(cmd)
 			if err != nil {
 				if strings.Contains(err.Error(), "no buildable Go source files") {