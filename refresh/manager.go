c8c5377055694177db4bfeb0701da9280e16e31c
Mark Bates <mark@markbates.com>
2017-07-25 14:39:10 -0400

    override the default `go` path with ENV

--- refresh/manager.go
+++ refresh/manager.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/gobuffalo/envy"
 )
 
 type Manager struct {
@@ -85,7 +86,7 @@ func (r *Manager) build(event fsnotify.Event) {
 
 			now := time.Now()
 			r.Logger.Print("Rebuild on: %s", event.Name)
-			cmd := exec.Command("go", "build", "-v", "-i", "-o", r.FullBuildPath(), r.BuildTargetPath)
+			cmd := exec.Command(envy.Get("GO_BIN", "go"), "build", "-v", "-i", "-o", r.FullBuildPath(), r.BuildTargetPath)
 			err := r.runAndListen(cmd)
 			if err != nil {
 				if strings.Contains(err.Error(), "no buildable Go source files") {
