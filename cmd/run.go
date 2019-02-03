369a95c1a0ceff2eb8c7420c3bef2c88003c0219
Chase Hutchins <syntaqx@gmail.com>
2019-02-03 14:49:52 -0700

    allow for the configuration to be loaded from multiple sane defaults

--- cmd/run.go
+++ cmd/run.go
@@ -2,12 +2,16 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
 
 	"github.com/markbates/refresh/refresh"
 	"github.com/spf13/cobra"
 )
+
+// ErrConfigNotExist is returned when a configuration file cannot be found.
+var ErrConfigNotExist = errors.New("no config file was found")
 
 func init() {
 	RootCmd.AddCommand(runCmd)
@@ -29,19 +33,48 @@ func Run(cfgFile string) {
 
 func RunWithContext(cfgFile string, ctx context.Context) {
 	c := &refresh.Configuration{}
-	err := c.Load(cfgFile)
-	if err != nil {
-		log.Fatalln(err)
-		os.Exit(-1)
+
+	if err := loadConfig(c, cfgFile); err != nil {
+		if err != ErrConfigNotExist {
+			log.Fatalln(err)
+			os.Exit(-1)
+		} else {
+			log.Println("No configuration loaded, proceeding with defaults")
+		}
+	}
+
+	if len(c.Path) > 0 {
+		log.Printf("Configuration loaded from %s\n", c.Path)
 	}
 
 	if debug {
 		c.Debug = true
 	}
+
 	r := refresh.NewWithContext(c, ctx)
-	err = r.Start()
-	if err != nil {
+	if err := r.Start(); err != nil {
 		log.Fatalln(err)
 		os.Exit(-1)
 	}
+}
+
+func loadConfig(c *refresh.Configuration, path string) error {
+	if len(path) > 0 {
+		return c.Load(path)
+	}
+
+	for _, f := range [4]string{
+		".refresh.yml",
+		".refresh.yaml",
+		"refresh.yml",
+		"refresh.yaml",
+	} {
+		err := c.Load(f)
+		if err != nil && os.IsNotExist(err) {
+			continue
+		}
+		return err
+	}
+
+	return ErrConfigNotExist
 }
